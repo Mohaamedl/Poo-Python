80f6621f67ffacf140c83d9f2504a1bb442d6a90
Mohamed <mohamedhadodo33@gamil.com>
2023-02-23 13:52:15 +0000

    i4

---
+++ Guiao1/Lib.py
@@ -0,0 +1,71 @@
+class Book:
+    id = 99
+    def __init__(self,*args):
+        if len(args) ==1 :
+            Book.id +=1
+            self.id = Book.id
+            self.title = args[0]
+            self.loanType = "NORMAL"
+        elif len(args) == 2:
+            Book.id +=1
+            self.id = Book.id
+            self.title = args[0]
+            self.loanType = args[1]
+    def __str__(self):
+        return f'Book {self.id}: {self.title}, {self.loanType}'
+    def getId(self):
+        return self.id
+    def getTitle(self):
+        return self.title
+    def setTitle(self,nTitle):
+        self.title = nTitle
+    def getLoanType(self):
+        return self.loanType
+    def setLoanType(self,newLoan):
+        self.loanType = newLoan
+class Student:
+    def __init__(self,name:str,nMec:int,course:str):
+        self.nMec = nMec
+        self.name = name
+        self.course = course
+    def __str__(self):
+        return f'Student: {self.nMec}; {self.name}; {self.course}'
+    def getNMec(self):
+        return self.nMec
+    def getName(self):
+        return self.name
+    def getCourse(self):
+        return self.course
+    def setNMec(self,newN):
+        self.nMec = newN
+    def setCourse(self,newC):
+        self.course = newC
+''' 
+livro1 = Book('edede','CONDICIONAL')
+liv2 = Book('fsfsef')
+liv3 = Book('batatas')
+print(f'{livro1} \n{liv2}\n{liv3}')
+s1 = Student(24234,'blabla','bliblibli')
+s2 =  Student(67238,'fifi','gigigigigi')
+print(f'{s1}\n{s2}')'''
+def main():
+    livro1= Book("Java 8", "CONDICIONAL");
+    print(livro1)
+    catalogo=[]
+    catalogo.append(livro1)
+    catalogo.append(Book("POO em Java 8"))
+    catalogo.append(Book("Java para totós", "NORMAL"))
+    
+    print(f"ID = {catalogo[1].getId()} Título = { catalogo[1].getTitle()}")
+    catalogo[2].setLoanType("CONDICIONAL")
+    for livro in catalogo:
+        print(livro)
+    # Students
+    utilizadores=[]
+    utilizadores.append(Student("Catarina Marques", 80232, "MIEGI"))
+    utilizadores.append(Student("Joao Silva", 90123, "Lic Física"))
+    utilizadores[1].setNMec(80123)
+    utilizadores.append(Student("António Costa", 100123, "Lic Matemática"))
+    for aluno in utilizadores:
+        print(aluno)
+main()
--- Guiao1/Shapes.py
+++ Guiao1/Shapes.py
@@ -1,3 +1,5 @@
+import turtle
+import math 
 from point import Point
 class Shapes:
     def __init__(self,color:str,center:Point):
@@ -12,3 +14,99 @@ class Shapes:
     def setColor(self,col):
         self.color = col
     
+
+
+class Rectangle(Shapes):
+    def __init__(self,color,point,sideA,sideB):
+        self.sideA = sideA
+        self.sideB = sideB
+        super().__init__(color,point)
+
+    def getSideA(self):
+        return self.sideA
+    def getSideB(self):
+        return self.sideB
+
+    def setSideA(self,A):
+        self.sideA = A
+    def setSideB(self,B):
+        self.sideA = B
+    def calArea(self):
+        return self.sideA*self.sideB
+    def calPer(self):
+        return self.sideA*2 + self.sideB*2
+    def draw(self):
+        turtle.goto(self.center.getX(),self.center.getY())
+        turtle.pendown()
+
+        turtle.speed(5)
+        turtle.pensize(10)
+        turtle.pencolor(self.color) 
+        turtle.forward(self.sideA)
+        turtle.left(90)
+        turtle.forward(self.sideB)
+        turtle.left(90)
+        turtle.forward(self.sideA)
+        turtle.left(90)
+        turtle.forward(self.sideB)
+        turtle.penup()
+
+
+class Square(Shapes):
+    def __init__(self,color,center,side):
+        self.side=side
+        super().__init__(color,center)
+    def getSide(self):
+        return self.side
+    def setSide(self,s):
+        self.side = s
+    def calArea(self):
+        return self.side**2
+    def calPer(self):
+        return self.side*4
+    def draw(self):
+        turtle.goto(self.center.getX(),self.center.getY())
+        turtle.pendown()
+
+        turtle.speed(5)
+        turtle.pensize(self.side/15)
+        turtle.pencolor(self.color) 
+        turtle.forward(self.side)
+        turtle.left(90)
+        turtle.forward(self.side)
+        turtle.left(90)
+        turtle.forward(self.side)
+        turtle.left(90)
+        turtle.forward(self.side)
+        turtle.penup()
+
+class Circle(Shapes):
+    def __init__(self,color,center,radius:float):
+        assert radius>=0, "radius must be positive number!"
+        self.radius = radius
+        super().__init__(color,center)
+    def __str__(self):
+        return f'Circle: radius = {self.radius}, center = {self.center}, color = {self.color}'
+    def getRadius(self):
+        return self.radius
+    def setRadius(self,r):
+        self.radius = r
+    def calArea(self):
+        return self.radius**2*math.pi
+    def calPer(self):
+        return 2*math.pi*self.radius
+    def draw(self):
+        turtle.goto(self.center.getX(),self.center.getY()) 
+        turtle.pendown()
+        turtle.speed(self.radius/8)
+        turtle.pensize(self.radius/10)
+        turtle.pencolor(self.color) 
+        turtle.circle(self.radius)
+        turtle.penup()
+    def intersect(self,c):
+        if self.center.distance(c.center)<=self.radius+c.radius:
+            return True
+        else:
+            return False
+
+
